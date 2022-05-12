6cdeb135e928da2646b93560d965659473d95e24
srihariyati(●'◡'●) <sri.hr@mhs.unsyiah.ac.id>
2022-05-12 16:31:25 +0700

    Delete file

--- dayahku/urls.py
+++
@@ -1,8 +0,0 @@
-from xml.etree.ElementInclude import include
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('santri.urls')),
-    path('loginUstad/', include('ustad.urls')),
-
-]
